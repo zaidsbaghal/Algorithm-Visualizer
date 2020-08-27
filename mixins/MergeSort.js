6e89a6ec12cadbde9afcae93b68ddb2dbf3a5e7b
Zaid Baghal <4283944+zaidsbaghal@users.noreply.github.com>
2020-08-27 15:39:32 -0400

    merge sort animation init

--- mixins/MergeSort.js
+++ mixins/MergeSort.js
@@ -8,33 +8,43 @@ export default {
 
             return [mainArray, animations]
         },
-
         // Merge helper function
         mergeSortHelper: function (mainArray, start, end, auxillaryArray, animations) {
             if (start === end) return;
             const middle = Math.floor((start + end) / 2)
-
-            this.mergeSortHelper(auxillaryArray, start, middle, mainArray, animations);
-            this.mergeSortHelper(auxillaryArray, middle + 1, end, mainArray, animations);
+            animations.push(["curr", middle])
+            this.mergeSortHelper(auxillaryArray, start, middle, mainArray, animations); // split array in half
+            this.mergeSortHelper(auxillaryArray, middle + 1, end, mainArray, animations); // split array in half
             this.doMerge(mainArray, start, middle, end, auxillaryArray, animations);
         },
-
         doMerge: function (mainArray, start, middle, end, auxillaryArray, animations) {
             let k = start;
             let i = start;
             let j = middle + 1
             while (i <= middle && j <= end) {
+                animations.push(["comp", i, j]);
                 if (auxillaryArray[i] <= auxillaryArray[j]) {
-                    mainArray[k++] = auxillaryArray[i++];
+                    animations.push(["swap", k, i, auxillaryArray[i]]) // update swap colors
+                    mainArray[k] = auxillaryArray[i];
+                    i += 1;
                 } else {
-                    mainArray[k++] = auxillaryArray[j++];
+                    animations.push(["swap", k, j, auxillaryArray[j]]) // update swap colors
+                    mainArray[k] = auxillaryArray[j];
+                    j += 1;
                 }
+                k += 1;
             }
             while (i <= middle) {
-                mainArray[k++] = auxillaryArray[i++];
+                animations.push(["swap", k, i, auxillaryArray[i]]) // update swap colors
+                mainArray[k] = auxillaryArray[i];
+                i += 1;
+                k += 1
             }
             while (j <= end) {
-                mainArray[k++] = auxillaryArray[j++];
+                animations.push(["swap", k, j, auxillaryArray[j]]) // update swap colors
+                mainArray[k] = auxillaryArray[j];
+                j += 1
+                k += 1
             }
         }
     }
